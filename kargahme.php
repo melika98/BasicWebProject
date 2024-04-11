8788389c145853b9ca9d1eaa97aca75cdffbbd5d
melika98 <84496327+melika98@users.noreply.github.com>
2024-04-11 06:34:29 +0330

    Add files via upload

---
+++ kargahme.php
@@ -0,0 +1,60 @@
+<html>
+	<head>
+		<title>
+			 Melika Derakhshani
+		</title>
+		<link rel="stylesheet" type="text/css" href="kargahcssm.css">
+	</head>
+	<body>
+		<h1>Melika Derakhshani</h1>
+		<img src="melika.jpg" width="160" height="200" border="2">
+		<h2> About me</h2>
+		<div>My name is Melika and my last name is Derakhshani.
+			I was born in Mahdishahr city in Semnan province.
+			I have a diploma in experimental sciences.
+			I am studying Undergraduate in computer engineering. 
+		</div>
+		<h2>Skills and interest</h2>
+		<ul>
+			<font class="list">
+			<li>I am interested in the field of artificial intelligence and its combination with medical science and<br>neuroscience.
+			And I do a lot of research in this area and I also have ideas in this area. Of course, in<br>order to do that, 
+			I need to gain more skills in various fields.</li>
+			<li>I am currently fluent in C ++. And I'm learning Python.</li>
+			<li>I am also interested in teaching and for some time in high school I taught biology and physics to students.</li>
+			<li>In addition to scientific work, I am also interested in swimming and traditional Iranian music</li>
+			<li>So far I have been able to fully learn to swim. And soon I will learn to play the tambourine, which is<br>
+			the oldest Iranian stringed instrument.</li>
+			<li>I am very interested in skiing and mountaineering and I am very adventurous.I am going to join the group<br>
+			that conquers Damavand every year in their training and one day I will conquer Damavand with them.</li>
+			</font>
+		</ul>
+		<p>&nbsp &nbsp In general, I am interested in different sciences and heights, and I am full of excitement and adventure<p>
+		<h2>Links</h2>
+		<font class="list">
+		<ul>
+			<li><a href="https://semnan.ac.ir/">Semnan university</a>
+			<li><a href="https://ieeexplore.ieee.org">Engineering articles</a>
+			<li><a href="deeplearning.ai">Intriducing artificial intelligence technologoies</a>
+			<li><a href="https://www.coursera.org/">Different courses in mathematics, computer science, economoics and etc.</a>
+			<li><a href="https://ocw.mit.edu/index.htm">Courses and tutorials held at MIT</a>
+		</ul>
+		</font>
+		<h2>Social media</h2>
+		<ul>
+			<font class="list">
+			<li><a href="https://www.instagram.com/invites/contact/?i=y93o37u6exnl&utm_content=jwpe9gh">Instagram </a>
+			<li><a href="https://www.melika.derakhshani98@gmail.com">Gmail </a>
+		</ul>
+		</font>
+		<h3>Disscotion</h3>
+		<font class="Disscotion">
+		<form  action="diss.php" method="post">
+			Name: <input type="text" name="name"><br><br>
+			E-mail: <input type="text" name="email" ><br><br>
+			Comment: <textarea name="comment" rows="5" cols="50"></textarea><br><br>
+			<input type="submit">
+		</form>
+		</font>
+	</body>
+</html>
